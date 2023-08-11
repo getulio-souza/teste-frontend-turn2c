7afdd58df8d2e78da7b2597d664c93b6f7ea3ebf
getulio.souza <getulio.souza@evolutix.com.br>
2023-08-11 17:02:55 -0300

    feature: home-done-v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+interface cards{
+  name: string,
+  info:string
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'teste_turn2C';
+
+
+  //remover os objetos abaixo depois de fazer a conexao com a api
+  cards: cards[] = [
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+    {
+      name: "Chinook",
+      info: "12 - 15 anos"
+    },
+  ]
 }
+
