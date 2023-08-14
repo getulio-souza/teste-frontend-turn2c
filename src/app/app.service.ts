fd2f0bb518af412473de2c28d875f1ac828ca5d7
getulio.souza <getulio.souza@evolutix.com.br>
2023-08-14 15:29:46 -0300

    feature: service-updated

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,8 +12,12 @@ export class AppService {
     'x-api-key': 'live_SxyZtuzzz7VdKyJvgu03stLDO0FI0hG48ZxLTePPAhkxSyAH5SJ3Ux3cTjRv9LzR'
   });
 
-  url = ' https://api.thedogapi.com/v1/images/search?limit=8&has_breeds=1&page=0';
-  url2 = 'https://api.thedogapi.com/v1/images/?limit=10&page=0'
+  // url = ' https://api.thedogapi.com/v1/images/search?limit=8&has_breeds=1&page=0';
+  // url2 = 'https://api.thedogapi.com/v1/images/?limit=10&page=0'
+  // postUrl = 'https://api.thedogapi.com/v1/images/upload'
+  // getRaceUrl = 'https://api.thedogapi.com/v1/breeds?limit=10'
+
+  apiUrl = 'https://api.thedogapi.com'
 
   constructor(
     private http: HttpClient
@@ -21,23 +25,29 @@ export class AppService {
 
 
   //HOME
+
+  //get all dogs (home)
   getAllDogsHome(): Observable<any>{
-    return this.http.get(this.url, {headers : this.header})
+    return this.http.get(this.apiUrl + `/v1/images/search?limit=8&has_breeds=1&page=0`, {headers : this.header})
   }
 
-  //FAZER DEPOIS PARA FAZER O FILTRO FUNCIONAR!!!
-  // getByDogRace(): Observable<any>{
-  //   return this.http.get(this.url )
-  // }
+  //select by race
+  getDogByRace( breed_ids: number): Observable<any>{
+    return this.http.get(this.apiUrl + `/v1/breeds?limit=10/${breed_ids}`, {headers : this.header})
+  }
 
   //PROFILE
 
+  //get all dogs (profile)
   getAllDogsProfile(): Observable<any>{
-    return this.http.get(this.url2, {headers : this.header})
+    return this.http.get(this.apiUrl + `/v1/images/?limit=10&page=0`, {headers : this.header})
   }
 
-  // uploadDog(): Observable<any>{
-  //   return this.http.post(this.url2, {headers: this.header})
-  // }
+  //upload new dog
+  uploadNewDog(image: File): Observable<any>{
+    const formData = new FormData();
+    formData.append('image', image);
+    return this.http.post(this.apiUrl + `/v1/images/upload`, formData, {headers: this.header});
+  }
 }
 
