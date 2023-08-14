c4b98993fdeb3ea0ad50f11f05262fd7f757e560
getulio.souza <getulio.souza@evolutix.com.br>
2023-08-14 15:29:31 -0300

    feature: model-created

---
+++ src/app/model/dogs.ts
@@ -0,0 +1,14 @@
+import { ImageLoader } from "@angular/common";
+
+export class cards{
+  id?: number;
+  name?: string;
+  life_span?: string;
+  url?: string;
+}
+
+export class races {
+  id?: number;
+  name?: string;
+  bred_for?: string;
+}
